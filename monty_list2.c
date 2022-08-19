7bb597992d5cfaa2908efff10a9dd243bf3f8dd0
DaisyGeraldine <chipanal@hotmail.com>
2022-08-19 12:30:59 -0500

    modify conditional if in caso de NULL

--- monty_list2.c
+++ monty_list2.c
@@ -8,7 +8,7 @@
  */
 void monty_pint(stack_t **stack, unsigned int line_number)
 {
-	if (!stack)
+	if (*stack == NULL || !stack)
 	{
 		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
